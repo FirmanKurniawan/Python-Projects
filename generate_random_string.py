598093f9da02e0310e050a99580091b3867117da
SaputraHxr <91870671+SaputraHxr@users.noreply.github.com>
2021-10-20 21:58:52 +0700

    Add files via upload

---
+++ generate_random_string.py
@@ -0,0 +1,13 @@
+#Coded by Putra
+#Hacktoberfest2021
+import random
+import string
+
+def get_random_string(length):
+    letters = string.ascii_uppercase
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    print("Random string of length", length, "is:", result_str)
+
+get_random_string(8)
+get_random_string(8)
+get_random_string(6)
