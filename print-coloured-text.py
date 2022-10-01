d6c19a7d13112e01e2bde7be29264a76fe9dc8d3
firmanesia <95009533+firmanesia@users.noreply.github.com>
2022-10-01 13:59:58 +0700

    Create print-coloured-text.py

---
+++ print-coloured-text.py
@@ -0,0 +1,7 @@
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print(Fore.BLUE+Back.YELLOW+"Hi My name is Firmanesia "+ Fore.YELLOW+ Back.BLUE+"I am your Machine Learning Instructor")
+print(Back.CYAN+"Hi My name is Aman Kharwal")
+print(Fore.RED + Back.GREEN+ "Hi My name is Aman Kharwal")
