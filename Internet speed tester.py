e04ce08a8fdf14c30d19786ac3d1d581de5477a8
ajruttam <ajruttam@gmail.com>
2022-10-04 23:54:10 +0530

    Modified Internet speed tester.py

--- Internet speed tester.py
+++ Internet speed tester.py
@@ -1,20 +1,37 @@
 import speedtest
 st = speedtest.Speedtest()
 
-#menu
-print("----- Internet speed test -----")
-option = int(input('''- What speed would you like to test?
- - 1.Download speed
- - 2.Upload speed
-- Your choice --> '''))
-
-#download speed test
-if option == 1:
+def download():
     print("-",st.download()/1000000,"Mbps.")
 
-#upload speed test
-elif option == 2:
+def upload():
     print("-", st.upload()/1000000,"Mbps.")
 
-else:
-    print("- Invalid choice!")
+print("----- Internet speed test -----")
+#menu
+while True:
+    option = int(input('''- What speed would you like to test?
+     - 1.Download speed
+     - 2.Upload speed
+    - Your choice --> '''))
+
+    #download speed test
+    if option == 1:
+        download()
+        nextOption = input("- Would you like to check the upload speed? (Y/N): ").lower()
+        if nextOption == 'y':
+            upload()
+        print("Thank you!")
+        break
+
+    #upload speed test
+    elif option == 2:
+        upload()
+        nextOption = input("- Would you like to check the download speed? (Y/N): ").lower()
+        if nextOption == 'y':
+            download()
+        print("Thank you!")
+        break
+
+    else:
+        print("- Invalid choice! Please try again..")
