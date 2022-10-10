0ca8122cd0adbfeb4243983b06f11898d1ef0040
akmalfad <90227586+akmalfad@users.noreply.github.com>
2022-10-11 00:45:28 +0700

    Create mhsiswa.py

---
+++ mhsiswa.py
@@ -0,0 +1,12 @@
+class mahasiswa:
+    nama = "Muhammad Akmal Fadillah"
+    def details(self):
+        print("NIM: 023422345")
+        print("Fakultas: Arsitektur")
+        print("Angkatan: 2021")
+
+mhs = mahasiswa()
+
+print("Nama:",mhs.nama)
+
+mhs.details()
