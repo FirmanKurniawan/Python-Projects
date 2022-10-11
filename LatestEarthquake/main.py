6e47cb7e2697b483e96f85c65a4f4d0abf2a1f4b
Kholid0803 <kholidsaif312@gmail.com>
2022-10-12 05:47:06 +0700

    add latest earthquake python project

---
+++ LatestEarthquake/main.py
@@ -0,0 +1,10 @@
+"""
+Aplikasi Gempa Terkini
+MODULARISASI DENGAN FUNCTION
+MODULASRISASI DENGAN PACKAGE
+"""
+import gempaterkini
+
+if __name__ == '__main__':
+    result = gempaterkini.ekstraksi_data()
+    gempaterkini.tampilkan_data(result)
