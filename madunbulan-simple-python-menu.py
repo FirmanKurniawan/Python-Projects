da1e1ce3730c939fc5110a70f1d257d63d238de0
madunbulan <85464475+madunbulan@users.noreply.github.com>
2022-10-15 19:55:23 +0700

    simple python menu

---
+++ madunbulan-simple-python-menu.py
@@ -0,0 +1,20 @@
+while True:
+    print("""
+    1. option 1
+    2. option 2
+    3. option 3
+    4. option 4
+    """)
+    
+    choice = input("Choose an option: ")
+    
+    if choice == "1":
+        print("you chose option 1")
+    elif choice == "2":
+        print("you chose option 2")
+    elif choice == "3":
+        print("you chose option 3")
+    elif choice == "4":
+        print("you chose option 4")
+    else:
+        print("That's not a valid option!")
