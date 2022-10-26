845d11860969f62aa32e7d77f8193410a6da58b6
lordacil <rizkynugrahaherliyanto28@gmail.com>
2022-10-26 23:01:26 +0700

    Menambahkan program perhitungan python

---
+++ Perhitungan/hitung_akar-kuadrat.py
@@ -0,0 +1,8 @@
+# Memasukkan Inputan Angka
+angka = float(input('Tuliskan Angka: '))
+ 
+# Menghitung Akar Kuadrat
+akar_kuadrat = angka ** 0.5
+ 
+#Menampilkan Hasil Akar Kuadrat
+print('Akar Kuadrat dari %0.3f adalah %0.3f'%(angka ,akar_kuadrat))
---
+++ Perhitungan/hitung_volume-kubus.py
@@ -0,0 +1,8 @@
+# Menginput Sisi Kubus
+sisi = float(input('Tulis Sisi Kubus: '))
+ 
+# Hitung Volume Kubus
+volume = sisi ** 3
+ 
+#Menampilkan Hasil Perhitungan
+print('Volume Kubus adalah %0.2f' %volume)
